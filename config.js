5d1737494f7b6059973eb44f19ea446cc813f1d0
charisTheo <haristh@hotmail.com>
2019-05-04 12:45:59 +0100

    remove cred

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ let config = {};
 
 config.PORT = process.env.PORT || 3000;
 
-config.client_id = credentialsJson["client_id"]; //920868000545-fns7i3dih3dpphol6er62cfot8i86g8u.apps.googleusercontent.com
+config.client_id = credentialsJson["client_id"];
 config.project_id = credentialsJson["project_id"];
 config.auth_uri = credentialsJson["auth_uri"];
 config.token_uri = credentialsJson["token_uri"];
 config.auth_provider_x509_cert_url = credentialsJson["auth_provider_x509_cert_url"];
-config.client_secret = credentialsJson["client_secret"]; // 5tYogkOqfy0miEFCQY06BJjI
+config.client_secret = credentialsJson["client_secret"];
 config.auth_callback_url = credentialsJson["auth_callback_url"];
 
 config.google_photos_album_url = `https://photoslibrary.googleapis.com/v1/albums`
